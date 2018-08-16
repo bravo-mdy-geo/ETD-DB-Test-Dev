762023a0c5b67be4f8e7e1d5a2f5a47c3ae77aa8
MyoMyoCU <septembermyo96@gmail.com>
2018-08-16 10:47:33 +0630

    add html files for nav

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+	var objRef = firebase.database().ref().child("2010");
+
+	objRef.on("child_added", snap => {
+		var name = snap.child("Name").val();
+		var major = snap.child("Major").val();
+		var degree = snap.child("Degree").val();
+		var supervisor = snap.child("SupervisorName").val();
+		var thesistitle = snap.child("ThesisTitle").val();
+		var abstract = snap.child("Abstract").val();
+
+		//alert(team);*/
+		//console.log(snap.child("ThesisTitle").val());
+
+		//var title = snap.child("ThesisTitle").val();
+		//$("#div_title").append("<h3>" + thesistitle + "</h3>");
+
+		//$("#div_abstract").append(abstract);
+
+		$("#div_table_tbody").append("<tr><td>" + name + "</td><td>" + major + "</td><td>" + degree + 
+			"</td><td>" + supervisor + "</td><td>" + thesistitle + "</td><td>" + abstract + "</td></tr>" );
+
+	});
+	//document.getElementById("thesis").setText(objRef.child("Team").value);
+
+});
